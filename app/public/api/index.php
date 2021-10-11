86c35732aef13aeb65ef4849668adf8d25e0b143
willaust <willaust@iu.edu>
2021-10-11 19:57:17 -0400

    Update index.php

--- app/public/api/index.php
+++ app/public/api/index.php
@@ -1,30 +1,21 @@
 <?php
-require 'class/DbConnection.php';
+require 'DbConnection.php';
 
 // Step 1: Get a datase connection from our helper class
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM offer';
+$sql = 'SELECT * FROM books';
 $vars = [];
 
-if (isset($_GET['student'])) {
-  // This is an example of a parameterized query
-  $sql = 'SELECT * FROM offer WHERE studentId = ?';
-
-  //NOT THIS WAY
-  // $sql = 'SELECT * FROM offer WHERE studentId = ' . $_GET['student'];
-
-  $vars = [ $_GET['student'] ];
-}
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$offers = $stmt->fetchAll();
+$books = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($offers, JSON_PRETTY_PRINT);
+$json = json_encode($books, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
