e0fac52194f77765c2805128c70582a2fe25ef20
willaust <willaust@iu.edu>
2021-10-20 12:49:07 -0400

    Update

--- app/public/create.php
+++ app/public/create.php
@@ -1,10 +1,8 @@
 <?php
-
 // if (($_SERVER['REQUEST_METHOD'] ?? '') != 'POST') {
 //     header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed");
 //     exit;
 // }
-
 try {
     $_POST = json_decode(
                 file_get_contents('php://input'), 
@@ -18,16 +16,12 @@ try {
     // echo file_get_contents('php://input');
     exit;
 }
-
 require("class/DbConnection.php");
-
 // Step 0: Validate the incoming data
 // This code doesn't do that, but should ...
 // For example, if the date is empty or bad, this insert fails.
-
 // Step 1: Get a datase connection from our helper class
 $db = DbConnection::getConnection();
-
 // Step 2: Create & run the query
 // Note the use of parameterized statements to avoid injection
 $stmt = $db->prepare(
@@ -40,16 +34,15 @@ $stmt->execute([
   $_POST['author'],
   $_POST['yearpublished'],
   $_POST['publisher'],
-  $_POST['pagecount'],
+  $_POST['pagecount']
   $_POST['msrp']
 ]);
 
 // Get auto-generated PK from DB
 // https://www.php.net/manual/en/pdo.lastinsertid.php
 // $pk = $db->lastInsertId();  
-
 // Step 4: Output
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
 header('HTTP/1.1 303 See Other');
-header('Location: ../public');
+header('Location: ../offer/?student=' . $_POST['studentId']);
